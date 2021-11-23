ccad76ecaba7911e94930e253e3bd3119207a3eb
canicemichael <canicemichael@gmail.com>
2021-11-23 22:41:27 +0100

    vidly setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { endpoint, masterKey, port } = require('./config');
+const genres = require('./routes/genres');
+
+const app = express();
+
+mongoose.connect("mongodb://localhost/vidly_app2", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDb'))
+    .catch(err => console.error('Error!.. connection wasn\'t made... ', err))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/genres', genres);
+
+app.listen(port, () => {
+    console.log(`Listening to http://localhost:${port}`);
+});
